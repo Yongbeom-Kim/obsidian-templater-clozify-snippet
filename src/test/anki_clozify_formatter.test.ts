c4d11ae8d39501b0e76ede98b6ef8dec364b29a6
Dernbu <63487502+Dernbu@users.noreply.github.com>
2022-10-10 00:41:10 +0800

    Tests for all bulletted anki conversions

--- src/test/anki_clozify_formatter.test.ts
+++ src/test/anki_clozify_formatter.test.ts
@@ -22,38 +22,108 @@ test("- <a> - <b> becomes - c1::::{{<a>}} - c1::{{<b>}} ", () => {
     );
 });
 
-test("indented 1. a - b", () => {
-    expect(parseTextLine("    1. hello world - hello", 1)).toEqual({
-        result: '1. c1::::{{hello world}} - c1::{{hello}}',
-        clozeNumber: 2,
-        state: 0
-    })
-});
-test("", () => {
+test("- <a> = <b> becomes - c1::::{{<a>}} = c1::{{<b>}} ", () => {
     expect(parseTextLine("- hello world = hello", 1)).toEqual({
         result: '- c1::::{{hello world}} = c1::{{hello}}',
         clozeNumber: 2,
-        state: 0
-    })
-});
-test("", () => {
+        state: STATE.TEXT
+    });
+
     expect(parseTextLine("    - hello world = hello", 1)).toEqual({
         result: '- c1::::{{hello world}} = c1::{{hello}}',
         clozeNumber: 2,
         state: 0
     })
+
+    expect(parseTextLine("- asd=sd", 1)).toEqual(
+        { result: '- asd=sd', clozeNumber: 1, state: 0 }
+    );
+
+    expect(parseTextLine("      - asdf=sd", 1)).toEqual(
+        { result: '      - asdf=sd', clozeNumber: 1, state: 0 }
+    );
 });
-test("", () => {
+
+test("1. <a> - <b> becomes 1. c1::::{{<a>}} - c1::{{<b>}} ", () => {
+    expect(parseTextLine("1. hello world - hello", 1)).toEqual({
+        result: '1. c1::::{{hello world}} - c1::{{hello}}',
+        clozeNumber: 2,
+        state: STATE.TEXT
+    });
+    
+    expect(parseTextLine("102. hello world - hello", 1)).toEqual({
+        result: '102. c1::::{{hello world}} - c1::{{hello}}',
+        clozeNumber: 2,
+        state: STATE.TEXT
+    });
+
+    expect(parseTextLine("    1. hello world - hello", 1)).toEqual({
+        result: '1. c1::::{{hello world}} - c1::{{hello}}',
+        clozeNumber: 2,
+        state: 0
+    });
+
+    expect(parseTextLine("    124. hello world - hello", 1)).toEqual({
+        result: '124. c1::::{{hello world}} - c1::{{hello}}',
+        clozeNumber: 2,
+        state: 0
+    });
+
+    expect(parseTextLine("1. asd-sd", 1)).toEqual(
+        { result: '1. asd-sd', clozeNumber: 1, state: 0 }
+    );
+
+    expect(parseTextLine("152. asd-sd", 1)).toEqual(
+        { result: '152. asd-sd', clozeNumber: 1, state: 0 }
+    );
+
+    expect(parseTextLine("      1. asdf-sd", 1)).toEqual(
+        { result: '      1. asdf-sd', clozeNumber: 1, state: 0 }
+    );
+
+    expect(parseTextLine("      194. asdf-sd", 1)).toEqual(
+        { result: '      194. asdf-sd', clozeNumber: 1, state: 0 }
+    );
+});
+
+test("1. <a> = <b> becomes 1. c1::::{{<a>}} = c1::{{<b>}} ", () => {
     expect(parseTextLine("1. hello world = hello", 1)).toEqual({
         result: '1. c1::::{{hello world}} = c1::{{hello}}',
         clozeNumber: 2,
-        state: 0
-    })
-});
-test("", () => {
+        state: STATE.TEXT
+    });
+    
+    expect(parseTextLine("102. hello world = hello", 1)).toEqual({
+        result: '102. c1::::{{hello world}} = c1::{{hello}}',
+        clozeNumber: 2,
+        state: STATE.TEXT
+    });
+
     expect(parseTextLine("    1. hello world = hello", 1)).toEqual({
         result: '1. c1::::{{hello world}} = c1::{{hello}}',
         clozeNumber: 2,
         state: 0
-    })
+    });
+
+    expect(parseTextLine("    124. hello world = hello", 1)).toEqual({
+        result: '124. c1::::{{hello world}} = c1::{{hello}}',
+        clozeNumber: 2,
+        state: 0
+    });
+
+    expect(parseTextLine("1. asd-sd", 1)).toEqual(
+        { result: '1. asd-sd', clozeNumber: 1, state: 0 }
+    );
+
+    expect(parseTextLine("152. asd-sd", 1)).toEqual(
+        { result: '152. asd-sd', clozeNumber: 1, state: 0 }
+    );
+
+    expect(parseTextLine("      1. asdf-sd", 1)).toEqual(
+        { result: '      1. asdf-sd', clozeNumber: 1, state: 0 }
+    );
+
+    expect(parseTextLine("      194. asdf-sd", 1)).toEqual(
+        { result: '      194. asdf-sd', clozeNumber: 1, state: 0 }
+    );
 });
