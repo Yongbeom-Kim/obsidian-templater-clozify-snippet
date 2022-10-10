de054fa848b991c73917e81765731928c8859622
Dernbu <63487502+Dernbu@users.noreply.github.com>
2022-10-10 10:48:16 +0800

    docs for utils

--- src/parser/LatexParser.ts
+++ src/parser/LatexParser.ts
@@ -1,12 +1,12 @@
 import { ParseOutput, STATE } from "./Parser";
 import { parseLine as parseTextLine } from "./TextParser";
+import { getSubStringAfter } from "../util/utils"
 export function parseLine(line: string, clozeNumber: number): ParseOutput {
     // Means there is an end of the latex equation here
     if ((line).match(/\$\$/)) {
         // Parse rest of line with text
-        // We can use !. here cause we tested for match already
-        const lineAfterLatex: string = line.substring(/\$\$/.exec(line)!.index + 2);
-        const afterParsed = parseTextLine(lineAfterLatex, clozeNumber);
+        // Note that getSubStringAfter cannot be null here
+        const afterParsed = parseTextLine(getSubStringAfter(line, /\$\$/), clozeNumber);
         
         return {
             result: line + afterParsed.result,
--- src/util/utils.ts
+++ src/util/utils.ts
@@ -1,3 +1,10 @@
+/**
+ * Get the substring after the first match of a regular expression.
+ * Note: Check that the regular expression matches the string before using this method.
+ * @param str string to parse
+ * @param regExp regular expression
+ * @returns substring after if regex exists, null otherwise.
+ */
 export function getSubStringAfter(str: string, regExp: RegExp) {
     if (!regExp.test(str)) {
         return null;
@@ -7,6 +14,13 @@ export function getSubStringAfter(str: string, regExp: RegExp) {
     return str.substring(regMatchInfo!.index + regMatchInfo![0]!.length);
 }
 
+/**
+ * Get the substring before the first match of a regular expression.
+ * Note: Check that the regular expression matches the string before using this method.
+ * @param str string to parse
+ * @param regExp regular expression
+ * @returns substring after if regex exists, str otherwise.
+ */
 export function getSubStringBefore(str: string, regExp: RegExp) {
     if (!regExp.test(str)) {
         return str;
